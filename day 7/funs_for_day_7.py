85524e897bd2f5f1606e850daff40d1120a965f3
BaratovAbdulaziz <AbdulazizBaratov12@gmail.com>
2024-08-15 21:03:13 +0500

    some changes

--- day 7/funs_for_day_7.py
+++ day 7/funs_for_day_7.py
@@ -54,3 +54,15 @@ sevemth = '''
   |  / \
  ---
 '''
+logo = '''I apologize for missing some elements. Let's enhance the ASCII art to include more game-related details. Here's an updated version:
+Copy     _____
+    |/    |     _    _    ____    _   _    ____    __  __    ____    _   _
+    |     |    | |  | |  / __ \  | \ | |  / __ \  |  \/  |  / __ \  | \ | |
+    |     O    | |__| | | |  | | |  \| | | |  | | | \  / | | |  | | |  \| |
+    |    /|\   |  __  | | |  | | | . ` | | |  | | | |\/| | | |  | | | . ` |
+    |    / \   | |  | | | |__| | | |\  | | |__| | | |  | | | |__| | | |\  |
+    |          |_|  |_|  \____/  |_| \_|  \____/  |_|  |_|  \____/  |_| \_|
+ ___|___       
+/       \      .<>-+=\/*_,."The Classic Word Guessing Game",.
+\________\     
+'''
